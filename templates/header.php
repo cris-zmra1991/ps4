9fafeb45af9f0fd8c1350ef2d424adb61683dd07
cris-zmra1991 <cris.zmra1991@gmail.com>
2021-08-08 18:33:05 -0400

    cambiado titulo de la pagina

--- templates/header.php
+++ templates/header.php
@@ -2,6 +2,6 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Gestion de la Produccion</title>
+    <title>Control de la Produccion</title>
     <link rel="stylesheet" href="../res/css/main.css">
   </head>
