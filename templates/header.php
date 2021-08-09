4caf4cbf7f48ac9f60a89c15db18cb609a39b45b
cris-zmra1991 <cris.zmra1991@gmail.com>
2021-08-08 20:20:04 -0400

    eliminados espacios en blanco

--- templates/header.php
+++ templates/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>Control de la Produccion</title>
-    <link rel="stylesheet" href="../res/css/main.css">
-  </head>
+<head>
+  <meta charset="utf-8">
+  <title>Control de la Produccion</title>
+  <link rel="stylesheet" href="../res/css/main.css">
+</head>
