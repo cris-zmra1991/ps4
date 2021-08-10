62c6f104c1957ddadbf7800de6303a23847785e4
cris-zmra1991 <cris.zmra1991@gmail.com>
2021-08-10 17:30:49 -0400

    cambios en css vinculados

--- templates/header.php
+++ templates/header.php
@@ -3,5 +3,6 @@
 <head>
   <meta charset="utf-8">
   <title>Control de la Produccion</title>
-  <link rel="stylesheet" href="../res/css/main.css">
+  <link rel="stylesheet" href="../res/css/semantic.min.css">
+  <link rel="stylesheet" href="../res/css/icon.min.css">
 </head>
