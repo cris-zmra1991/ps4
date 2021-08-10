64d17a4edc637a6826af6eee5910c50d370cbdde
cris-zmra1991 <cris.zmra1991@gmail.com>
2021-08-10 17:27:15 -0400

    Agregada conexion a BD

--- base.php
+++ base.php
@@ -1,5 +1,18 @@
 <?php
-define('RUTA', 'http://localhost:8030/p4/');
+define('RUTA', 'http://localhost:8030/ps4/');
 require 'config/session_handler.php';
 require 'config/medoo.php';
+use Medoo\Medoo;
+try {
+  $database = new Medoo([
+  	// [required]
+  	'type' => 'mysql',
+  	'host' => 'localhost',
+  	'database' => 'cp',
+  	'username' => 'root',
+  	'password' => '',
+   ]);
+} catch (PDOException $e) {
+  echo "No se pudo conectar a la Base de Datos";
+}
 ?>
