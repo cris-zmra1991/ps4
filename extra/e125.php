c0e3c4d9c9347aaae5073c7c92fc0274329dc7e3
cris-zmra1991 <cris.zmra1991@gmail.com>
2021-08-13 00:28:05 -0400

    agregada pagina de error de permisos

---
+++ extra/e125.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="../res/css/semantic.min.css">
+    <link rel="stylesheet" href="../res/css/icon.min.css">
+  </head>
+  <body>
+    <br><br>
+    <div class="ui middle aligned center aligned grid">
+    <h1 class="ui header">
+      <i class="huge ban icon"></i> Usted no tiene permiso para realizar esta acción.</h1>
+      <br><h1 class="ui header">Contacte al administrador</h1>
+    </div>
+  </body>
+</html>
